a84f423c736f12e7ce2cf5363c6965bbb9cc2606
syedfaran4 <154878224+faran4@users.noreply.github.com>
2025-04-07 10:08:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const userRoutes = require("./routes/userRoutes");
 const noticeRoutes = require("./routes/noticeRoutes");
 
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:5173', 
-    credentials: true
-  }));
+
+app.use(cors()); 
+// app.use(cors({
+//     origin: 'http://localhost:5173', 
+//     credentials: true
+//   }));
 app.use(express.json());
 
 // Base route for Render testing
